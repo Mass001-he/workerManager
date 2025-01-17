80e118d3f10eae58265128bc5f1fd84eefd95724
mass <heqiang@snto.com>
2025-01-17 21:07:57 +0800

    --

--- src/db/utils/index.ts
+++ src/db/utils/index.ts
@@ -1,21 +1,3 @@
-import { OpfsSAHPoolDatabase } from '@sqlite.org/sqlite-wasm';
-
-export function createTable(db: OpfsSAHPoolDatabase, sql: string) {
-  db.exec(sql);
-}
-
-export interface ISchemaObj {
-  [index: string]: Record<string, string>;
-}
-
-export function generateSchema(schemaObj: ISchemaObj): string {
-  let schema = Object.entries(schemaObj).reduce((acc, [key, value]) => {
-    return acc + `${key} ${value.sqlType},`;
-  }, '');
-
-  return schema;
-}
-
 export function isOK(result: any[]) {
   return result.length > 0;
 }
